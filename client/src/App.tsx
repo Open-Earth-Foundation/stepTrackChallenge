4ba9059e9b81af947f2054992cc87add60349258
oefuxteam <25716522-oefuxteam@users.noreply.replit.com>
2025-05-08 20:02:19 +0000

    Restored to '873dbb72032bbf4041b0fa0ece54e610d2fe2f7f'
    Replit-Restored-To: 873dbb72032bbf4041b0fa0ece54e610d2fe2f7f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/Dashboard";
-import Landing from "@/pages/Landing";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Landing} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/" component={Dashboard} />
       {/* Fallback to 404 */}
       <Route component={NotFound} />
     </Switch>
